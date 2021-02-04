f1b6a8ad3876c8f77005b3a20472c80d8df0f802
Henrique Dias <hacdias@gmail.com>
2021-02-04 23:08:07 +0100

    add ftypes

--- zsh/.oh-my-zsh/custom/plugins/hacdias/aliases.zsh
+++ zsh/.oh-my-zsh/custom/plugins/hacdias/aliases.zsh
@@ -5,6 +5,7 @@ alias l='ls -CF'
 alias open-ports="lsof -Pan -iTCP -sTCP:LISTEN"
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias fresh-install="rm -r node_modules && rm package-lock.json && npm install"
+alias ftypes="find . -type f | awk -F'.' '{print $NF}' | sort| uniq -c | sort -g"
 
 # Testground
 alias tg='testground'
