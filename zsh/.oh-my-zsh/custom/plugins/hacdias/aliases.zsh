b7fffcd860a9b7b33f5690a32875f58bbb534f22
Henrique Dias <hacdias@gmail.com>
2021-05-28 23:43:32 +0200

    move alias to right place

--- zsh/.oh-my-zsh/custom/plugins/hacdias/aliases.zsh
+++ zsh/.oh-my-zsh/custom/plugins/hacdias/aliases.zsh
@@ -6,6 +6,7 @@ alias open-ports="lsof -Pan -iTCP -sTCP:LISTEN"
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias fresh-install="rm -r node_modules && rm package-lock.json && npm install"
 alias ftypes="find . -type f | awk -F'.' '{print \$NF}' | sort| uniq -c | sort -g"
+alias swap_yubikey="killall gpg-agent && rm -rf ~/.gnupg/private-keys-v1.d/ && gpg --card-status"
 
 # Testground
 alias tg='testground'
@@ -15,5 +16,3 @@ alias tgb='testground build'
 
 # Uni Stuff
 alias cdmsce='cd "~/Google Drive/MSCE"'
-
-alias swap_yubikey="killall gpg-agent && rm -rf ~/.gnupg/private-keys-v1.d/ && gpg --card-status"
