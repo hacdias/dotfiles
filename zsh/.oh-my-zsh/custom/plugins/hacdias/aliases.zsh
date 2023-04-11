9bf5d58a1b43f948324556542b56f746afa699ce
Henrique Dias <hacdias@gmail.com>
2023-04-11 13:55:06 +0200

    add jaeger alias

--- zsh/.oh-my-zsh/custom/plugins/hacdias/aliases.zsh
+++ zsh/.oh-my-zsh/custom/plugins/hacdias/aliases.zsh
@@ -8,3 +8,15 @@ alias ftypes="find . -type f | awk -F'.' '{print \$NF}' | sort| uniq -c | sort -
 alias swap_yubikey="killall gpg-agent && rm -rf ~/.gnupg/private-keys-v1.d/ && gpg --card-status"
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 alias delete_empty='find . -type d -empty -delete'
+alias jaeger='docker run --rm -it --name jaeger \
+  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
+  -p 5775:5775/udp \
+  -p 6831:6831/udp \
+  -p 6832:6832/udp \
+  -p 5778:5778 \
+  -p 16686:16686 \
+  -p 14268:14268 \
+  -p 14269:14269 \
+  -p 14250:14250 \
+  -p 9411:9411 \
+  jaegertracing/all-in-one'
