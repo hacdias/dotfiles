b334007c6259832982059c5cbffe91c9e4b94423
Henrique Dias <hacdias@gmail.com>
2023-06-19 19:45:10 +0200

    update nata.cafe

--- configs/dns/dnsconfig.js
+++ configs/dns/dnsconfig.js
@@ -42,27 +42,12 @@ D('h4c.pt', REG_NONE, DnsProvider(CLOUDFLARE),
 
 D('henriquedias.com', REG_NONE, DnsProvider(CLOUDFLARE),
   A('@', THOR_IP, CF_PROXY_ON),
-  A('www', THOR_IP, CF_PROXY_ON),
-  TXT('@', 'google-site-verification=jIEoWFXm813Y2EAD4GzvNHrf1uOWJmsWZlsF9E2UgsM')
+  A('www', THOR_IP, CF_PROXY_ON)
 )
 
 D('nata.cafe', REG_NONE, DnsProvider(CLOUDFLARE),
   A('@', THOR_IP, CF_PROXY_ON),
   A('www', THOR_IP, CF_PROXY_ON),
-  A('nitter', THOR_IP, CF_PROXY_ON),
   A('miniflux', THOR_IP, CF_PROXY_ON),
-  // Migadu
-  SRV('_autodiscover._tcp', 0, 1, 443, 'autodiscover.migadu.com.'),
-  SRV('_submissions._tcp', 0, 1, 465, 'smtp.migadu.com.'),
-  SRV('_imaps._tcp', 0, 1, 993, 'imap.migadu.com.'),
-  SRV('_pop3s._tcp', 0, 1, 995, 'pop.migadu.com.'),
-  CNAME('autoconfig', 'autoconfig.migadu.com.', CF_PROXY_OFF),
-  CNAME('key1._domainkey', 'key1.nata.cafe._domainkey.migadu.com.', CF_PROXY_OFF),
-  CNAME('key2._domainkey', 'key2.nata.cafe._domainkey.migadu.com.', CF_PROXY_OFF),
-  CNAME('key3._domainkey', 'key3.nata.cafe._domainkey.migadu.com.', CF_PROXY_OFF),
-  MX('@', 10, 'aspmx1.migadu.com.'),
-  MX('@', 20, 'aspmx2.migadu.com.'),
-  TXT('@', 'hosted-email-verify=r8nqtrxw'),
-  TXT('@', 'v=spf1 include:spf.migadu.com -all'),
-  TXT('_dmarc', 'v=DMARC1; p=quarantine;')
+  A('linkding', THOR_IP, CF_PROXY_ON)
 )
