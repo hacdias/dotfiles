58c0b386994dd6dd60785a856abdfe360aee40f9
Henrique Dias <mail@hacdias.com>
2024-06-21 21:18:59 +0200

    zsh: add plugins as submodules

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -6,7 +6,3 @@ set -euo pipefail
 # ZSH install instructions: https://github.com/ohmyzsh/ohmyzsh/wiki/Installing-ZSH
 # Oh-My-Zsh install instructions: https://ohmyz.sh/#install
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install some plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
